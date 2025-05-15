19772750d238fb9d0e3f27c0f1158866a2ab1083
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-05-15 14:04:37 +0100

    commit

--- src/estateApp/data/repository/Residents.java
+++ src/estateApp/data/repository/Residents.java
@@ -1,4 +1,27 @@
 package estateApp.data.repository;
 
-public class Residents {
-}
+import estateApp.data.model.Resident;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Residents implements ResidentRepository {
+    private List<Resident> residents = new ArrayList<>();
+    private int currentId = 0;
+
+    @Override
+    public Resident save(Resident resident) {
+        if(isNew(resident))saveNew(resident);else update(resident);
+        return resident;
+    }
+
+    public void update(Resident resident) {
+        for(int count = 0; count < residents.size(); count++){
+            if(residents.get(count).getId() == resident.getId()){
+                residents.set(count, resident);
+                return;
+            }
+        }
+    }
